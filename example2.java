47beec261ca5187d8f198ba7fd8bff43b0f02252
Verified ✔️ <ryesan58@gmail.com>
2022-09-09 00:35:32 -0400

    Create example2.java
    learning variables with java

---
+++ example2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class example2 {
+  public static void main(String[] args) {
+    Scanner scnr = new Scanner(System.in);
+    int count;
+    int x;
+    count = scnr.nextInt();
+    x = count + 4;
+    System.out.println(count);
+    System.out.println(x);
+    System.out.println(count + 5);
+    
+    
+  }
+}
