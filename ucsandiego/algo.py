5d90ae938c9279a13cd259ff2db80eb955732a94
Verified ✔️ <ryesan58@gmail.com>
2022-09-27 08:11:31 -0400

    Update algo.py

--- ucsandiego/algo.py
+++ ucsandiego/algo.py
@@ -1,4 +1,5 @@
 from math import prod
+from tokenize import Double
 
 
 x = []
@@ -28,8 +29,68 @@ print(product())
 
 def squared():
     x=[]
-    for i in range(1300, 2000):
-        print(f"{i} times {i} == ", i*i)
 
 
 squared()
+
+
+def product2():
+    x = []
+    for i in range(0,100):
+      if i%3 == 2 and i%4 == 3 and i%5 == 4:
+          x.append(i)
+    return x
+
+print(product2())
+
+def isnt_hundred():
+    count = 0
+    for i in range(10, 100):
+        for x in range(i,100):
+            if i+x == 100 and i !=x:
+                count += 1
+    return 90-count
+
+
+
+print(isnt_hundred())
+# Choose the maximal number of integers among 1..50 that can be selected if we are not allowed to select n and 2n at at same time.
+
+def no_doubles():
+    x = []
+    y=[]
+    doubles = []
+    for i in range(1,51):
+        x.append(i)
+    for i in x:
+        if i not in doubles:
+            y.append(i)
+            doubles.append(2*i)
+    return y 
+
+print(no_doubles())
+
+#recusive functions
+
+def factorial(x):
+    if x == 1:
+      return x
+    else:
+        return x * factorial(x-1)
+
+print(factorial(5))
+
+#Change can be made
+
+def changer(change):
+    if change == 8:
+        return [3,5]
+    if change == 9:
+        return [3,3,3]
+    if change == 10:
+        return [5,5]
+    answer= changer(change-3)
+    answer.append(3)
+    return answer
+
+print(changer(3))
