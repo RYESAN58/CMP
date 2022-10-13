c23029e0dbf690fbaf4dceb35414c5d43cacd7ad
Verified ✔️ <ryesan58@gmail.com>
2022-10-12 23:13:01 -0400

    Create EvenNumbers.java

---
+++ EvenNumbers.java
@@ -0,0 +1,10 @@
+public class EvenNumbers {
+  public static void main(String[] args) {
+    int count= 3;
+    
+    while(count < 50){
+      System.out.println(count);
+      count = count +2; 
+    }
+  }
+}
