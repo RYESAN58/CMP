125d55f286195e84be6446792720c8921a4c72ab
Verified ✔️ <ryesan58@gmail.com>
2022-11-08 02:41:06 -0500

    Update Homework6Methods.java

--- Homework6Methods.java
+++ Homework6Methods.java
@@ -14,9 +14,9 @@ public class Homework6Methods {
     }
   }
   public static int getMaxOf3Ints(int val1, int val2, int val3) {
-    if (val1 > val2  && val1 > val3){
+    if (val1 >= val2  && val1 >= val3){
       return val1;
-    }else if(val2 > val1 && val2 > val3){
+    }else if(val2 >= val1 && val2 >= val3){
       return val2;
     }else{
       return val3;
@@ -25,45 +25,62 @@ public class Homework6Methods {
   public static int getMedianOf3Ints (int val1, int val2, int val3){
     int x = getMaxOf3Ints(val1, val2, val3);
     int y = printMinOf3Ints(val1, val2, val3);
-    int fin = 0;
+    System.out.println("the max is " + x);
+    System.out.println(val1);
+    System.out.println(val2);
+    System.out.println(val3);
+    
     if(val1 != x && val1 != y){
       return val1;
     }else if(val2 != x && val2 != y){
       return val2;
-    }else{
+    }else if(val3 != x && val3 != y){
       return val3;
+    }else{ 
+      return val1;
     }
     
   }
   public static int printMinOf3Ints(int val1, int val2, int val3) {
-    if (val1 < val2  && val1 < val3){
+    if (val1 <= val2  && val1 <= val3){
       System.out.println("The min is " + val1);
       return val1;
-    }else if(val2 < val1 && val2 < val3){
+    }else if(val2 <= val1 && val2 <= val3){
       System.out.println("The min is " + val2);
       return val2;
-    }else{
+    }else if(val3 <= val1 && val3 <= val2){
       System.out.println("The min is " + val3);
       return val3;
+    }else{
+      System.out.println("The min is " + val2);
+      return val2;
     }
   }
   public static int getProdOfAllPositiveInts(int x){
-    int y=1;
-    for(int i = x; i>=1; i--){
-      y= y*i;
-    }
+    int y=0;
+    if (x > 0){
+      y = 1;
+      for(int i = 1; i<= x; i++){
+        y= y*i;
+        }
+        return y;
+      }
     return y;
   }
   public static int getProdOfAllNegativeInts(int x){
-    int y= -1;
-    if(x >= 0){
+    int y = -1;
+    if (x>=0){
       return 0;
-    }else{
-      for(int i = x; i>= 0; i++){
-        y = 3;
-      }
-      return y;
     }
+    for(int i = x; i <= -1; i++){
+        if(i == x){
+          y = x;
+        }else {
+           y *= i;
+        }
+    
+    }
+    return y;
   };
   public static char getCharAtIndex(String s, int x){
     char y = '?';
@@ -93,13 +110,17 @@ public class Homework6Methods {
     return newString;
   }
   public static String getStringTitleCased (String s){
+    if (s.length() == 0){
+      return "";
+    }
     s = s.toLowerCase();
 
     s = s.substring(0, 0) + Character.toUpperCase(s.charAt(0)) + s.substring(0 + 1);
 
-    for(int i =0; i < s.length(); i++){
+    for(int i = 0; i < s.length(); i++){
       if(s.charAt(i) == ' '){
         int first = i+1;
+        // char letter = s.charAt(first);
         s = s.substring(0, first) + Character.toUpperCase(s.charAt(first)) + s.substring(first + 1);
       }
     }
@@ -107,6 +128,6 @@ public class Homework6Methods {
     return s;
   }
   public static void main(String[] args) {
-    getStringTitleCased("RYAN IS THE GOAT  ");
+
   }
 }
