11b5d58a525b3268d9fc91af381484518bb1682f
Verified ✔️ <ryesan58@gmail.com>
2022-11-24 02:37:57 -0500

    Update Grades.java

--- arrays/Grades.java
+++ arrays/Grades.java
@@ -1,4 +1,4 @@
-// package arrays;
+package arrays;
 import java.util.Scanner;
 
 public class Grades {
@@ -28,7 +28,7 @@ public class Grades {
   }
   public static int maxValue(int[] grades, int firstIndex, int secondIndex){
     int count = 0;
-    if(firstIndex < 0 || secondIndex < 0 || firstIndex > grades.length || secondIndex > grades.length || secondIndex < firstIndex){
+    if(firstIndex < 0 || secondIndex < 0 || firstIndex >= grades.length || secondIndex >= grades.length || secondIndex < firstIndex){
       return -666;
     }
     for(int i =firstIndex; i <= secondIndex; i++){
@@ -65,14 +65,12 @@ public class Grades {
     return count;
   }
   public static int minValue(int[] grades, int firstIndex, int secondIndex){
-    int count = 0;
-    if(firstIndex < 0 || secondIndex < 0 || firstIndex > grades.length || secondIndex > grades.length || secondIndex < firstIndex){
+    int count = grades[0];
+    if(firstIndex < 0 || secondIndex < 0 || firstIndex >= grades.length || secondIndex >= grades.length || secondIndex < firstIndex){
       return -666;
     }
     for(int i =firstIndex; i <= secondIndex; i++){
-      if (i == 0){
-        count = grades[i];
-      }else if (grades[i] < count){
+      if (grades[i] < count){
         count = grades[i];
       }
     }
@@ -87,7 +85,7 @@ public class Grades {
 }
   public static int sum(int[] arr, int firstIndex, int secondIndex){
     int x = 0;
-    if(firstIndex < 0 || secondIndex < 0 || firstIndex > arr.length || secondIndex > arr.length || secondIndex < firstIndex){
+    if(firstIndex < 0 || secondIndex < 0 || firstIndex >= arr.length || secondIndex >= arr.length || secondIndex < firstIndex){
       return -666;
     }
     if (firstIndex <= secondIndex){
@@ -107,7 +105,6 @@ public class Grades {
     }
     double dsum = sum;
     double dcount = count;
-    System.out.println(dcount+ " " + dsum);
     x = dsum/dcount;
     return x;
   }
@@ -129,8 +126,73 @@ public class Grades {
     }
     return x;
   }
-  
 
+  public static int numberOfBelowAverageElements(int[] arr){
+    double x = average(arr);
+    
+    int count =0;
+    for(int i =0; i < arr.length; i++){
+      if(arr[i] < x){
+        count +=1;
+      }
+    }
+    // int value = (int) count;
+    return count;
+  }
+
+  public static int numberOfAboveAverageElements(int[] arr){
+    double x = average(arr);
+    int count =0;
+    for(int i =0; i < arr.length; i++){
+      if(arr[i] > x){
+        
+        count +=1;
+      }
+    }
+    // int value = (int) count;
+    return count;
+  }
+  public static void rotateElements(int[] arr){
+    int temp = arr[arr.length-1];
+    for(int i=arr.length-1; i > -1; i--){
+      if(i != 0){
+        arr[i] = arr[i-1];
+      }else{
+        arr[i] = temp;
+      }
+      System.out.println(arr[i]);
+    }
+  }
+  public static void rotateElements(int[] arr, int rotationCount ){
+    int[] x = new int[arr.length];
+    for(int i =0; i < arr.length; i++){
+      if(i + rotationCount <= arr.length-1){
+        x[i+rotationCount] = arr[i];
+      }else if(i + rotationCount > arr.length-1){
+        int y = (i+rotationCount) - (arr.length);
+        x[y] = arr[i];
+      }
+    }
+    for(int i =0; i < arr.length; i++){
+      arr[i] = x[i];
+    }
+  }
+
+  public static void reverseArray(int[] arr){
+    int[] x = new int[arr.length];
+    int index = 0;
+    while(index < arr.length-1){
+      for(int i=arr.length-1; i > -1; i--){
+        x[index] = arr[i];
+        // System.out.println(x[index]);
+        index++;
+      }
+    }
+    for(int i=0; i< arr.length;i++){
+      arr[i] = x[i];
+      System.out.print(arr[i]);
+    }
+  }
 
 
   public static void main(String[] args) {    
@@ -145,8 +207,9 @@ public class Grades {
     //   }
     //   System.out.println("");
     // }
-    int[] x = {1,2,3};
+    int[] x = {45,22,18,89,82,79,15,69,100,55,48,72,16,98,57,75,44,32,21,14,7,16,49,58,72};
 
-    System.out.print(indexOfFirstMaxValue(x));
+    // System.out.print(numberOfBelowAverageElements(x));
+    rotateElements(x,6);
   }
 }
