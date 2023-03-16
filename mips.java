63f5281005d01838de4c110631325e6170af5eb9
Verified ✔️ <ryesan58@gmail.com>
2023-03-16 17:08:26 -0400

    Create mips.java

---
+++ mips.java
@@ -0,0 +1,19 @@
+public class mips {
+
+  public static void main(String[] args) {
+    int s0 = 5;
+    int s1 =  9;
+    int s2 = 8;
+    int t0 =  0;
+    int x;
+    if(s0 < s1){
+      t0 = 1;
+    }else{
+      t0 = 0;
+    }
+    if(t0 == 1){
+      // x = 
+    }
+  }
+
+}
