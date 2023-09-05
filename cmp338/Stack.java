9c9550d4fc38f3e8db40775fa8f0d9b1ef2b9b5a
Verified ✔️ <ryesan58@gmail.com>
2023-09-05 19:41:05 -0400

    starting stacks

---
+++ cmp338/Stack.java
@@ -0,0 +1,33 @@
+
+package cmp338;
+
+
+public class Stack {
+  private int  top = -1;
+  private int[] A = new int [8];
+
+  // isEmpty
+  public boolean isEmpty() {
+    return top == -1;
+  }
+  // push
+  public  void push(int x) {
+    top++;
+    A[top] = x;
+  }
+  // pop
+  public void pop() {
+    top--;
+  }
+
+  // top
+  public int top() {
+    return A[top];
+  }
+  
+
+  
+  public static void main(String[] args) {
+
+  }
+}
