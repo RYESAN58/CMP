630b7dc2ad81b02f05e2cf5c4bbd18211329a16b
Verified ✔️ <ryesan58@gmail.com>
2023-09-19 22:11:19 -0400

    Update UsingStacks.java

--- cmp338/UsingStacks.java
+++ cmp338/UsingStacks.java
@@ -3,25 +3,24 @@ package cmp338;
 public class UsingStacks {
   public static int postfixEval(String postFix){
     Stack stack = new Stack((postFix.length()/2)+1);
-    for(int i=0; i < postFix.length()-1; i++){
+    for(int i=0; i < postFix.length(); i++){
       char op = postFix.charAt(i);
-      
-      if((op=='0') || (op=='1') ||(op=='2') ||(op=='3') ||(op=='4') ||(op=='5') ||(op=='6') ||(op=='7') ||(op=='8') ||(op=='9')){
-        
-        stack.push( op - '0');
-      }
-      if((op== '*' ||op== '/' ||op== '-' ||op== '+')){
+    
+      if(isOperator(op)){
         int b = stack.top();   stack.pop();
-        
         int a = stack.top();   stack.pop();
-        
         if(op == '*')     {stack.push(a*b);}
         if(op == '/')     {stack.push(a/b);}
         if(op == '+')     {stack.push(a+b);}
         if(op == '-')     {stack.push(a-b);}
+      }else{
+        stack.push( op - '0');
       }
     }
     return stack.top();
+  }
+  public static boolean isOperator(char op){
+    return (op == '*') || (op == '/') || (op == '-') || (op == '+');
   }
 
 
