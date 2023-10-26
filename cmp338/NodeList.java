3da5c2fa288055d6f88fc3d19bb51e5099351aa7
Verified ✔️ <ryesan58@gmail.com>
2023-10-26 19:08:54 -0400

    Create NodeList.java

---
+++ cmp338/NodeList.java
@@ -0,0 +1,22 @@
+package cmp338;
+
+public class NodeList {
+  private Node tail = new Node(0, null);
+  private Node head = new Node(0, tail);
+  private Node curr = new Node(0, null);
+  private Node prev = new Node(0, null);
+  
+
+
+
+  public void insertBefore (int value){
+    prev.next = new Node(0, prev.next);
+    curr = prev.next;
+  }
+
+
+  public void inserAfter (int value){
+    prev = curr;
+    insertBefore(value) ;
+  }
+}
