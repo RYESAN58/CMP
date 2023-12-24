0a99218958df4b8e8f5d538a42befdb5fdf61cc1
Verified ✔️ <ryesan58@gmail.com>
2023-12-24 17:58:23 -0500

    Update Node.java

--- cmp338/Node.java
+++ cmp338/Node.java
@@ -11,4 +11,26 @@ public class Node {
     this.next = next;
   }
   
+
+  public ListNode middleNode(ListNode head) {
+    int count = -1;
+    int position;
+    ListNode curr = head;
+    if(head.next == null){return head;}
+    while(curr.next != null){
+        count++;
+        curr = curr.next;
+    }
+    position = count/2+1;
+    count = 0;
+    curr = head;
+    while(curr.next != null){
+        count++;
+        curr = curr.next;
+        if(count == position){
+            return curr;
+        }
+    }
+    return null;
+  }
 }
